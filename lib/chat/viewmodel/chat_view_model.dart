94254fda80d40273707ed5fd07812caa64af55f0
maxxdroid <theonlymaxmc@gmail.com>
2025-06-21 15:30:28 +0000

    feat: Implement user data fetching and chatroom functionality
    This commit introduces the following changes:
    
    - Fetches user data from Firestore on initialization.
    - Retrieves the currently authenticated user's data.
    - Adds a method to open a chatroom with another user, including room ID generation and initialization.

--- lib/chat/viewmodel/chat_view_model.dart
+++ lib/chat/viewmodel/chat_view_model.dart
@@ -1,25 +1,52 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ern_u/chat/service/chat_service.dart';
+import 'package:ern_u/chat/view/chatroom_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class ChatViewModel extends GetxController {
   final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+  final ChatService chatService = ChatService();
   final RxList<Map<String, dynamic>> users = <Map<String, dynamic>>[].obs;
+  var authUser = <String, dynamic>{}.obs;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
     _fetchUsers();
+    _loadUserData();
+  }
+
+  Future<void> _loadUserData() async {
+    final user = _auth.currentUser;
+    if (user != null) {
+      final userDoc = await _fireStore.collection('users').doc(user.uid).get();
+      if (userDoc.exists) {
+        authUser.value = userDoc.data()!;
+      }
+    }
   }
 
   Future<void> _fetchUsers() async {
     try {
       final usersSnapshot = await _fireStore.collection('users').get();
       users.value = usersSnapshot.docs.map((doc) => doc.data()).toList();
-      print("User 1  ================ ${users[0]}");
+      Get.log("User details ${users[0]}");
     } catch (e) {
-      print('Error fetching users: $e');
+      Get.log('Error fetching users: $e');
     }
   }
+
+  Future<void> openChatroom(Map<String, dynamic> otherUser ) async {
+    try {
+      String roomId = chatService.getRoomId(authUser["userId"], otherUser["userId"]);
+      await chatService.initRoomIfNeeded(roomId: roomId, currentUser: authUser, otherUser: otherUser);
+      Get.to(() => ChatRoomView(roomId: roomId));
+    } catch (e) {
+      Get.log('Error opening chatroom: $e');
+    }
+  }
+
 
 }
